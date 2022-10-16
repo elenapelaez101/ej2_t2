e8c478ae000d2f19bd98581dcd4110f9d22e48d7
elenapelaez101 <elena.pelaezsanchez@usp.ceu.es>
2022-10-16 02:26:02 +0200

    ejercicio terminado

--- ejercicio.py
+++ ejercicio.py
@@ -11,14 +11,18 @@ def mayus (lista):
     return new
 def mods(listi):
     listi[0] = listi[0]+"..."
-    for l in listi[1:]:
-        l = l +"."
-    print(listi)
+    #for l in listi[1:]:
+        #l = l +"."
+        #no se por qué no funciona este codigo, asi que lo hago uno a uno
+    listi[1] = "-"+ listi[1]+"."
+    listi[2] = "-"+ listi[2]+"."
+    listi[3] = "-"+ listi[3]+"."
+    for i in listi:
+        print("\n"+ i)
 
         
     
 
 t= separar("un día que el viento soplaba con fuerza#mira como se mueve aquella banderola -dijo un monje#lo que se mueve es el viento -respondió otro monje#ni las banderolas ni el viento, lo que se mueve son vuestras mentes -dijo el maestro")
-print(t)
 u = mayus(t)
 mods(u)
